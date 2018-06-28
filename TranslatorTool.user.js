d9dd9e326df9ee1e1e4ef5115f52663cae753b35
Xingdong Zeng <xingdo@amazon.com>
2018-06-28 23:47:48 +0800

    add input element

--- TranslatorTool.user.js
+++ TranslatorTool.user.js
@@ -3,14 +3,19 @@
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
-// @author       You
+// @author       xingdo
 // @match        http://tampermonkey.net/stats.php?ext=dhdg
+// @require https://code.jquery.com/jquery-2.1.4.min.js
 // @include      http://translator.amazon.com/*
 // @grant        none
 // ==/UserScript==
+function HelperUI() {
+    $("body").append('<input type=file id="fileInput">');
+    alert("meme");
+}
 
 (function() {
     'use strict';
-
+     HelperUI();
     // Your code here...
 })();//IIFE(Immediately Invoked Function Expression)
